76eab507d3b58c6822cad2f8496dbc8197835839
Anthony Narvaez <aan266@nau.edu>
2025-03-14 13:35:25 -0700

    added public/ directory for server.js to serve static files from. Also updated server.js and .gitignore

--- Q-upSite/server.js
+++ Q-upSite/server.js
@@ -5,6 +5,7 @@ const PORT = 3000;
 // Serve static files (HTML, CSS, JS)
 app.use(express.static('public'));
 
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
