07760b1a32558bd3abe1bae073c7116e49623013
Anthony Narvaez <aan266@nau.edu>
2025-04-13 20:15:12 -0700

    Restructured routes to work with front end changes

--- Q-upSite/server.js
+++ Q-upSite/server.js
@@ -22,7 +22,7 @@ const PORT = 3000;
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',  // <-- TEMPORARY
-    password: 'Narvaez#042719',  // <-- TEMPORARY
+    password: 'password',  // <-- TEMPORARY
     database: 'QUp'  // <-- TEMPORARY
 })
 
@@ -34,6 +34,13 @@ connection.connect(err => {
     console.log("Successful MySQL Connection!");
 });
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+//Database interaction functions
+function addReservation(date, time, user) {
+    connection.execute(
+        'INSERT INTO reservations, '
+    )
+}
 
 
 // Defining Local Strategy For Authentication-----------------------------------
@@ -115,6 +122,18 @@ app.use(passport.session());
 //Authentication middleware
 app.use(express.urlencoded({ extended: false }));
 
+//Custom authentication middleware
+function isAuth(req, res, next) {
+    //Make sure the user session is valid
+    if (req.isAuthenticated ()) {
+        return next();
+    }
+
+    //if not valid session
+    res.redirect('/login');   //REPLACE WITH POPUP!!
+}
+
+
 
 //async functions
 async function registerUser(username, password) {
@@ -137,46 +156,103 @@ async function registerUser(username, password) {
 
 //routes------------------------------------------------------------------------
 
-//login routes     
-app.get("login", (req, res) => {
-    //serves the login page
+// //login routes      REFERENCE ROUTES!!
+// app.get("login", (req, res) => {
+//     //serves the login page
 
-    //THIS MIGHT BE DIFFERENT DUE TO UTLIZATION OF POPUP
-} )
+//     //THIS MIGHT BE DIFFERENT DUE TO UTLIZATION OF POPUP
+// } )
 
-app.post("/login", express.urlencoded({extended: false}), (req, res, next) => {
-    //authenticate the log in attempt
-    passport.authenticate('local', {
-        successRedirect: '/reserve',
-        failureRedirect: '/login',
-        failureFlash: true
-    })(req, res, next);
+// app.post("/login", express.urlencoded({extended: false}), (req, res, next) => {
+//     //authenticate the log in attempt
+//     passport.authenticate('local', {
+//         successRedirect: '/reserve',
+//         failureRedirect: '/login',
+//         failureFlash: true
+//     })(req, res, next);
+// })
+
+// //TODO: Make LOGOUT route
+
+// //TODO: reservation routes
+// app.get("/reserve", (req, res) => {
+//     //serve the "Make a Reservation" page, only if the user is logged in
+
+//     passport.authenticate('local', {
+//         successRedirect: '/reserve',
+//         failureRedirect: '/login',
+//         failureFlash: true
+//     })
+
+// })
+
+// app.post("/reserve", express.urlencoded({extended: false}), (req, res, next) =>{
+//     //save the new reservation request in the database
+// })
+
+// //temporary register endpoint for testing of user account creation
+// app.post('/register', async (req, res) => {
+//     const {username, password} = req.body
+//     await registerUser(username, password);
+//     res.send("User Registered!")
+// })
+
+//ACTUAL routes
+
+// --> Reservations
+    //POST request for authentication of the user and for adding a new reservation
+app.post("/availability-schedule", express.urlencoded({extended: false}), isAuth, async (req, res, next) => {
+
+    const user = req.user
+    try {
+        //querying database for user
+        await connection.execute(
+            'INSERT INTO reservations (id, date, start, end) VALUES (?, ?, ?, ?)',
+            [user.id, user.body.date, user.body.startTime, user.body.endTime]
+        );
+
+    }
+    catch(err) {
+        console.error("Reservation Error: ", err)
+        res.status(500).send("Error creating Reservation")
+    }
 })
 
-//TODO: Make LOGOUT route
 
-//TODO: reservation routes
-app.get("/reserve", (req, res) => {
-    //serve the "Make a Reservation" page, only if the user is logged in
+// --> Account creation
+    //POST request for making a new user
+app.post("/New_Account", express.urlenconded({extended: false}), async (req, res, next)=>{
+    
+    const user = req.body.username
+    const password = req.body.password
 
-    passport.authenticate('local', {
-        successRedirect: '/reserve',
-        failureRedirect: '/login',
-        failureFlash: true
-    })
-
+    
+    registerUser(username, password)
 })
 
-app.post("/reserve", express.urlencoded({extended: false}), (req, res, next) =>{
-    //save the new reservation request in the database
+    //GET request for accessing a user to make sure they don't already exist
+    //before account creation
+app.get("/New_Account", (req, res, next)=>{
+    try {
+        //querying database for user
+        const results = connection.execute(
+            'SELECT id, username FROM users WHERE username = ?',
+            [req.username]
+        );
+
+        //no user found
+        if(results.length === 0) {
+            return done(null, false)
+        }
+
+        //if user found, returning the user object
+        return done(null, results[0]);
+    }
+    catch(err) {
+        done(err)
+    }
 })
 
-//temporary register endpoint for testing of user account creation
-app.post('/register', async (req, res) => {
-    const {username, password} = req.body
-    await registerUser(username, password);
-    res.send("User Registered!")
-})
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
